1de5e51c5d9611d7ace6e53172dcd49a2696ed14
Dmitry Sovgyr <dmitry.s@techery.io>
2019-03-12 15:00:25 +0200

    Update Http Monitor screen

---
+++ example/lib/http/app_http_client.dart
@@ -0,0 +1,171 @@
+import 'dart:convert';
+import 'dart:io' show HttpClient, HttpClientRequest, HttpHeaders;
+
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:http/http.dart' as http;
+
+abstract class AppHttpClient {
+  Future<int> executeGet(String url);
+
+  Future<int> executeGetImage();
+
+  Future<int> executePost(String url);
+
+  Future<int> executeDelete(String url);
+
+  Future<int> executePut(String url);
+
+  Future<int> executePatch(String url);
+
+  Future<int> executeHead(String url);
+
+  Future<int> executeTrace(String url);
+
+  Future<int> executeOptions(String url);
+}
+
+class FlutterHttpClient extends AppHttpClient {
+  @override
+  Future<int> executeDelete(String url) {
+    return http.delete(url).then((response) {
+      return response.statusCode;
+    });
+  }
+
+  @override
+  Future<int> executeGet(String url) {
+    return http.get(url).then((response) {
+      return response.statusCode;
+    });
+  }
+
+  @override
+  Future<int> executeGetImage() {
+    return http
+        .get(
+            "https://raw.githubusercontent.com/appspector/android-sdk/master/images/github-cover.png")
+        .then((response) {
+      return response.statusCode;
+    });
+  }
+
+  @override
+  Future<int> executeHead(String url) {
+    return http.head(url).then((response) {
+      return response.statusCode;
+    });
+  }
+
+  @override
+  Future<int> executeOptions(String url) {
+    throw Exception("OPTION request is not supported by current client");
+  }
+
+  @override
+  Future<int> executePatch(String url) async {
+    final body = await rootBundle.loadString("assets/patch.json");
+    return http.patch(url, body: body).then((response) {
+      return response.statusCode;
+    });
+  }
+
+  @override
+  Future<int> executePost(String url) async {
+    final body = await rootBundle.loadString("assets/post.json");
+    return http.post(url, body: body).then((response) {
+      return response.statusCode;
+    });
+  }
+
+  @override
+  Future<int> executePut(String url) async {
+    final body = await rootBundle.loadString("assets/put.json");
+    return http.put(url, body: body).then((response) {
+      return response.statusCode;
+    });
+  }
+
+  @override
+  Future<int> executeTrace(String url) {
+    throw Exception("TRACE request is not supported by current client");
+  }
+}
+
+class IOHttpClient extends AppHttpClient {
+  final client = HttpClient();
+
+  @override
+  Future<int> executeDelete(String url) {
+    return _executeRequest(client.deleteUrl(Uri.parse(url)));
+  }
+
+  @override
+  Future<int> executeGet(String url) {
+    return _executeRequest(client.getUrl(Uri.parse(url)));
+  }
+
+  @override
+  Future<int> executeGetImage() {
+    return _executeRequest(client.getUrl(Uri.parse(
+        "https://raw.githubusercontent.com/appspector/android-sdk/master/images/github-cover.png")));
+  }
+
+  @override
+  Future<int> executeHead(String url) {
+    return _executeRequest(client.headUrl(Uri.parse(url)));
+  }
+
+  @override
+  Future<int> executeOptions(String url) {
+    return _executeRequest(client.openUrl("option", Uri.parse(url)));
+  }
+
+  @override
+  Future<int> executePatch(String url) {
+    return _executeRequestWithBody(
+        client.patchUrl(Uri.parse(url)), "assets/patch.json");
+  }
+
+  @override
+  Future<int> executePost(String url) {
+    return _executeRequestWithBody(
+        client.postUrl(Uri.parse(url)), "assets/post.json");
+  }
+
+  @override
+  Future<int> executePut(String url) {
+    return _executeRequestWithBody(
+        client.putUrl(Uri.parse(url)), "assets/put.json");
+  }
+
+  @override
+  Future<int> executeTrace(String url) {
+    return _executeRequest(client.openUrl("trace", Uri.parse(url)));
+  }
+
+  Future<int> _executeRequest(Future<HttpClientRequest> requestFuture) {
+    return requestFuture.then((request) {
+      return request.close();
+    }).then((response) {
+      response.transform(Utf8Decoder()).listen((data) {
+        print("Client IO has received: $data");
+      });
+      return response.statusCode;
+    });
+  }
+
+  Future<int> _executeRequestWithBody(
+      Future<HttpClientRequest> requestFuture, String bodyAssetName) async {
+    final body = await rootBundle.load(bodyAssetName);
+    return requestFuture.then((request) {
+      request.headers.add(HttpHeaders.contentTypeHeader, "application/json");
+      request.add(body.buffer.asUint8List());
+      return request.close();
+    }).then((response) {
+      response.transform(Utf8Decoder()).listen((data) {
+        print("Client IO has received: $data");
+      });
+      return response.statusCode;
+    });
+  }
+}
--- example/lib/http_page.dart
+++ example/lib/http_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+
 import 'app_drawer.dart';
-import 'package:http/http.dart' as http;
+import 'http/app_http_client.dart';
+import 'http/http_request_item.dart';
 
 class HttpMonitorPage extends StatefulWidget {
   @override
@@ -8,55 +10,159 @@ class HttpMonitorPage extends StatefulWidget {
 }
 
 class HttpMonitorPageState extends State<HttpMonitorPage> {
+  static const _CLIENT_HTTP_LIB = 0;
+  static const _CLIENT_IO = 1;
+
   final _url = "https://google.com";
+  final _flutterHttpClient = FlutterHttpClient();
+  final _ioHttpClient = IOHttpClient();
+
+  final List<HttpRequestItems> requestMethods = [
+    new HttpRequestItems("GET Request", (httpClient, url) {
+      return httpClient.executeGet(url);
+    }),
+    new HttpRequestItems("GET Image Request", (httpClient, _) {
+      return httpClient.executeGetImage();
+    }),
+    new HttpRequestItems("POST Request", (httpClient, url) {
+      return httpClient.executePost(url);
+    }),
+    new HttpRequestItems("DELETE Request", (httpClient, url) {
+      return httpClient.executeDelete(url);
+    }),
+    new HttpRequestItems("PUT Request", (httpClient, url) {
+      return httpClient.executePut(url);
+    }),
+    new HttpRequestItems("PATCH Request", (httpClient, url) {
+      return httpClient.executePatch(url);
+    }),
+    new HttpRequestItems("HEAD Request", (httpClient, url) {
+      return httpClient.executeHead(url);
+    }),
+    new HttpRequestItems("TRACE Request", (httpClient, url) {
+      return httpClient.executeTrace(url);
+    }),
+    new HttpRequestItems("OPTIONS Request", (httpClient, url) {
+      return httpClient.executeOptions(url);
+    })
+  ];
+
+  int _selectedClient = _CLIENT_HTTP_LIB;
   int _statusCode;
+  String _error;
   int _requestDuration;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("SQLite Monitor"),
-        ),
-        drawer: SampleAppDrawer(),
-        body: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 15.0),
-            child: Column(
-              children: <Widget>[
-                Container(
-                    alignment: Alignment.center,
-                    margin: EdgeInsets.only(top: 24.0),
-                    child: RaisedButton(
-                        child: Text("Execute HTTP Request"),
-                        onPressed: _executeHttpRequest)),
-                Container(
-                  margin: EdgeInsets.only(top: 24.0),
-                  child: Text(_statusCode == null
-                      ? ""
-                      : "Request finished with code: $_statusCode"),
+      appBar: AppBar(
+        title: Text("HTTP Monitor"),
+      ),
+      drawer: SampleAppDrawer(),
+      body: SingleChildScrollView(
+          child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15.0),
+              child: Column(children: <Widget>[
+                const Text(
+                    "Choose HTTP client:",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                        height: 2
+                    )
                 ),
+                Row(children: <Widget>[
+                  Expanded(
+                      child: RadioListTile(
+                          value: _CLIENT_HTTP_LIB,
+                          groupValue: _selectedClient,
+                          title: const Text("HTTP Library"),
+                          onChanged: _onClientSelectChanged)
+                  ),
+                  Expanded(child: RadioListTile(
+                      value: _CLIENT_IO,
+                      title: const Text("Client IO"),
+                      groupValue: _selectedClient,
+                      onChanged: _onClientSelectChanged))
+                ]),
                 Container(
-                  margin: EdgeInsets.only(top: 24.0),
-                  child: Text(
-                      _requestDuration == null ? "" : "$_requestDuration ms"),
+                    margin: EdgeInsets.only(top: 24.0),
+                    child: Text.rich(TextSpan(children: _createResultedText()))
+                ),
+                GridView.count(
+                  shrinkWrap: true,
+                  crossAxisCount: 2,
+                  primary: false,
+                  childAspectRatio: 3,
+                  children: createRequestMethodWidgetList(),
                 )
-              ],
-            )));
+              ])
+          )
+      ),
+    );
   }
 
-  _executeHttpRequest() {
-    debugPrint("Executing HTTP request");
-    Stopwatch stopwatch = Stopwatch()..start();
-    http.get(_url).then((result) {
-      _onHttpResponse(result, stopwatch.elapsedMilliseconds);
-      stopwatch.stop();
+  List<Widget> createRequestMethodWidgetList() {
+    return requestMethods.map((item) {
+      return Container(
+          alignment: Alignment.center,
+//          margin: EdgeInsets.only(top: 24.0),
+          child: RaisedButton(
+              child: Text(item.title),
+              onPressed: () {
+                Stopwatch stopwatch = Stopwatch()..start();
+                try {
+                  item.action(_provideClient(), _url).then((responseCode) {
+                    _onHttpResponse(
+                        responseCode, stopwatch.elapsedMilliseconds);
+                  });
+                } catch(e) {
+                  _onHttpError(e);
+                }
+              }));
+    }).toList();
+  }
+
+  AppHttpClient _provideClient() {
+    switch (_selectedClient) {
+      case _CLIENT_HTTP_LIB: return _flutterHttpClient;
+      case _CLIENT_IO: return _ioHttpClient;
+    }
+    throw Exception("Unknown client id");
+  }
+
+  _onHttpResponse(int statusCode, int requestDuration) {
+    setState(() {
+      _statusCode = statusCode;
+      _requestDuration = requestDuration;
+      _error = null;
     });
   }
 
-  _onHttpResponse(http.Response result, int requestDuration) {
+  _onHttpError(Exception e) {
     setState(() {
-      _statusCode = result.statusCode;
-      _requestDuration = requestDuration;
+      _statusCode = null;
+      _requestDuration = null;
+      _error = e.toString();
+    });
+  }
+
+  List<TextSpan> _createResultedText() {
+    if (_error != null) {
+      return [TextSpan(text: _error)];
+    }
+    if (_statusCode != null) {
+      List<TextSpan> lines = [];
+      lines.add(TextSpan(text: "Request finished with code: $_statusCode \n\n"));
+      lines.add(TextSpan(text: "$_requestDuration ms"));
+      return lines;
+    }
+    return [TextSpan(text: "Click any button")];
+  }
+
+  _onClientSelectChanged(int newValue) {
+    setState(() {
+      _selectedClient = newValue;
     });
   }
 }
