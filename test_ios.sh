d0afd4f98a9e65710add715482ddce6492f06699
Deszip <igor.asharenkov@techery.io>
2021-02-10 11:20:24 +0200

    fixed destination

--- test_ios.sh
+++ test_ios.sh
@@ -8,4 +8,4 @@ flutter pub get
 pod install
 
 # Build and test
-xcodebuild -workspace Runner.xcworkspace -scheme Runner -configuration Debug -sdk iphonesimulator -derivedDataPath /tmp/dd -destination "iPhone 12 Pro Max,OS=14.4" build test
+xcodebuild -workspace Runner.xcworkspace -scheme Runner -configuration Debug -sdk iphonesimulator -derivedDataPath /tmp/dd -destination "platform=iOS Simulator,name=iPhone 12 Pro Max,OS=14.4" build test
